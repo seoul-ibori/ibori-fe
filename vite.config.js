0af96324ad523f7836f16ad100bd0ba54ea65bd9
Jeong mokjin <jungmokjin@gmail.com>
2026-04-21 16:26:02 +0900

    🎉 Start: ibori 프로젝트 시작

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
