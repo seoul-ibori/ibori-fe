a5d91ec4e42dfdca5ad85749b03ebb2fedd9ec00
Jeong mokjin <jungmokjin@gmail.com>
2026-05-04 23:50:47 +0900

    🎨 Design: 질문 생성 배너 추가

--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -6,15 +6,19 @@ import MedicalRecord from '@/components/Main/MedicalRecord';
 
 export default function Main() {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col items-center py-1 gap-1">
+    <div className="flex flex-col py-5">
+      <section className="flex flex-col items-center py-1 gap-1">
         <MainPost />
         <LoginSuggestBanner />
-      </div>
+      </section>
       <Bar />
-      <div>
+      <section className="flex flex-col gap-4 px-6 py-5.5">
+        <div className="flex flex-col gap-1.25">
+          <p className="text-[15px] font-medium text-[#706963]">AI 아이 맞춤형 생성 질문지</p>
+          <p className="text-[18px] font-bold text-[#413B32]">진료 전 질문을 준비해요</p>
+        </div>
         <AICreateQuestion />
-      </div>
+      </section>
       <Bar />
       <div>
         <MedicalRecord />
