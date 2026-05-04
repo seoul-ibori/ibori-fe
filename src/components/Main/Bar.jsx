868f4c983c52b99b58baccdb691d5fd9d4de291c
Jeong mokjin <jungmokjin@gmail.com>
2026-05-04 23:17:40 +0900

    🎨 Design: 메인페이지 파일 추가 및 메인 게시물 구현

---
+++ src/components/Main/Bar.jsx
@@ -0,0 +1,3 @@
+export default function Bar() {
+  return <div className="h-3.75 w-full bg-[#FAF7F2]"></div>;
+}
--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -1,3 +1,24 @@
+import AICreateQuestion from '@/components/Main/AICreateQuestion';
+import Bar from '@/components/Main/Bar';
+import LoginSuggestBanner from '@/components/Main/LoginSuggestBanner';
+import MainPost from '@/components/Main/MainPost';
+import MedicalRecord from '@/components/Main/MedicalRecord';
+
 export default function Main() {
-  return <div className="">메인페이지</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center py-1">
+        <MainPost />
+        <LoginSuggestBanner />
+      </div>
+      <Bar />
+      <div>
+        <AICreateQuestion />
+      </div>
+      <Bar />
+      <div>
+        <MedicalRecord />
+      </div>
+    </div>
+  );
 }
