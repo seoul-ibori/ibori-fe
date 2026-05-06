2d5e97feeb1695fff91b295fdc920a2a2accd0c7
Jeong mokjin <jungmokjin@gmail.com>
2026-05-06 12:32:06 +0900

    🎨 Design: 스크롤바 숨김

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout() {
       <div className="relative mx-auto flex flex-col flex-1 w-full max-w-112.5 h-full bg-white shadow-lg">
         <div className="min-h-0 flex-1 flex flex-col">
           <Header />
-          <main className="flex-1 min-h-0 overflow-y-auto">
+          <main className="flex-1 min-h-0 overflow-y-auto no-scrollbar">
             <Outlet context={{ setIsModalOpen, isModalOpen, setIsLoading }} />
           </main>
           <NavBar />
